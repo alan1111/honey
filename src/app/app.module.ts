93fb1cf699cdf0ae06adc6e95c131d3fe927dd23
Alan Li <Alan.Li@habatrading.com>
2017-05-24 11:30:00 +0800

    *add one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { OneComponent } from './one/one.component';
+import {OneModule} from "./one/one.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OneComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    OneModule,
+    AppRoutingModule,
     HttpModule
   ],
   providers: [],
