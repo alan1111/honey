b24d8cb3b6f74b57b3c8dd87fbab31625545e194
Alan Li <Alan.Li@habatrading.com>
2017-05-24 11:30:49 +0800

    *add one module

---
+++ src/app/one/one-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {OneComponent} from "./one.component";
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'one',
+        component: OneComponent
+      },
+      {
+        path: '**',
+        redirectTo: '/one',
+        pathMatch: 'full'
+      }
+    ])
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class OneRoutingModule { }
